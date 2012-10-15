13d0a6d2ed4ea1331516007206b07838ce58b439
Mikko Jakonen <mikko.j.jakonen@gmail.com>
2012-10-15 12:30:03 +0300

    Making some of the buttons do something.

--- jquery.darkbox.js
+++ jquery.darkbox.js
@@ -5,6 +5,9 @@
         /** The main settings of Darkbox */
         var settings = $.extend({
             bookname    : null,
+            path        : "pages/",
+            extension   : ".jpg",
+            numPages    : 11,
             
             //keyboard shortcuts:
             keyHide     : 27,   //esc
@@ -37,6 +40,8 @@
             close       : '#DarkboxClose'
         }, options);
         
+        var currentPage = 1;
+        
         
         return initialize(this);
         
@@ -61,11 +66,11 @@
             html += '<div id="' + settings.page.substring(1) + '"><img id="' + settings.mainImage.substring(1) + '" src=""><img id="' + settings.loading.substring(1) + '" src="'+ settings.imgLoading + '"></div>';
             //navigation forward:
             html += '<a id="' + settings.next.substring(1) + '" href="javascript://"><img src="' + settings.imgNext + '"></a>' +
-            '<a id="' + settings.first.substring(1) + '" href="javascript://"><img src="' + settings.imgFirst + '"></a>';
+            '<a id="' + settings.last.substring(1) + '" href="javascript://"><img src="' + settings.imgLast + '"></a>';
             //close book:
             html += '</div>';
             //meta:
-            html += '<div id="' + settings.pagecount.substring(1) + '"><span id="' + settings.currentPage.substring(1) + '"></span> / <span id="' + settings.totalPages + '"></span></div>';
+            html += '<div id="' + settings.pagecount.substring(1) + '"><span id="' + settings.currentPage.substring(1) + '"></span> / <span id="' + settings.totalPages + '">' + settings.numPages + '</span></div>';
             //close:
             html += '<a href="javascript://"><div id="' + settings.close.substring(1) + '"></div></a>';
             //close overlay:
@@ -73,7 +78,36 @@
             
             elem.append(html);
             
+            //event bindings:
+            $(settings.first).click(function() { showPage(1); });
+            $(settings.last).click( function() { showPage(settings.numPages); });
+            $(settings.prev).click( function() { showPage(currentPage-1); });
+            $(settings.next).click( function() { showPage(currentPage+1); });
+            $(settings.close).click(hide);
+            
+            currentPage = 1;
+            showPage(currentPage);
             return elem;
+        } 
+        
+        /**
+         * Hides the Darkbox overlay.
+         */
+        function hide() {
+            $(settings.overlay).css("display", "none");
         }
+        
+        /**
+         * Swaps the page with the number pageNum as the currently viewed
+         * page.
+         */
+        function showPage(pageNum) {
+            currentPage = pageNum;
+            $(settings.currentPage).text(pageNum);    
+        
+        
+        }
+        
+        
     };
 })(jQuery);
